1d8741023c37df4024e480a3c22c2eed948dea7e
zeezu124 <zeezu124@gmail.com>
2022-05-28 19:05:34 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# Check if two words are anagrams 
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+
+
+def find_anagram(word, anagram):
+    # [assignment] Add your code here
+    if(sorted(word) == sorted(anagram)):
+        return True
+    else:
+        return False
